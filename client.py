c3cd6b01c9e1862a94bacb9c8bef8520cfa4d1fe
EC2 Default User <ec2-user@ip-172-31-33-70.us-east-2.compute.internal>
2019-08-18 06:08:49 +0000

    modified client.py

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+
 client.connect(('172.31.25.111', 8080))
-client.send("I am CLIENT\n")
-from_server = client.recv(4096)
+i = 0
+while True:  #i<100:
+    client.send("I am CLIENT\n")
+    from_server = client.recv(4096)
+    print(i, ".from_server")
+    i += 1
 client.close()
-print(from_server)
+
