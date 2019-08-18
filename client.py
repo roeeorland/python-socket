3959409cc707368afdc69b7e31c3867288dec494
roee <roee@cloudwize.io>
2019-08-18 08:43:28 +0300

    different tutorial

--- client.py
+++ client.py
@@ -1,11 +1,7 @@
 import socket
-
-HOST = '172.31.33.70'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('172.31.25.111', 8080))
+client.send("I am CLIENT\n")
+from_server = client.recv(4096)
+client.close()
+print(from_server)
--- server.py
+++ server.py
@@ -1,29 +1,15 @@
 import socket
-
-HOST = '172.31.25.111'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-#     with conn:
-#         print('Connected by', addr)
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             conn.sendall(data)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen()
-conn, addr = s.accept()
-with conn:
-    print('Connected by', addr)
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind(('0.0.0.0', 8080))
+serv.listen(5)
+while True:
+    conn, addr = serv.accept()
+    from_client = ''
     while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
-s.close()
+        data = conn.recv(4096)
+        if not data: break
+        from_client += data
+        print from_client
+        conn.send("I am SERVER\n")
+    conn.close()
+    print('client disconnected')
